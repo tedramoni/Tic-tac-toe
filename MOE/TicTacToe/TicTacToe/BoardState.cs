201c773ef65da6892670f6d1df611d584323d111
Thomas Negrault <thomas.negrault@ens.uvsq.fr>
2015-04-10 22:57:31 +0200

    Add Board state generation

---
+++ MOE/TicTacToe/TicTacToe/BoardState.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class BoardState
+    {
+        public string[] board;
+        public void generateBoard(int size = 3){
+            this.board = new string[size*size];
+            int cellNumber = 0;
+            for (int line = 1; line <= size; line++)
+            {
+                for (int row = 1; row <= size; row++)
+                {
+                    cellNumber = cellNumber + 1;
+                    this.board[cellNumber-1] = cellNumber.ToString();
+
+                }
+            }
+        }
+
+        public void updateBoard(int index, string value)
+        {
+            this.board[index-1] = value;
+        }
+
+    }
+}
