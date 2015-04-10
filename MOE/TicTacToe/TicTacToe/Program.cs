7dc64838b207cbd7273044796dbc184451b44e05
Thomas Negrault <thomas.negrault@ens.uvsq.fr>
2015-04-11 00:14:30 +0200

    Temporary main for testing

--- MOE/TicTacToe/TicTacToe/Program.cs
+++ MOE/TicTacToe/TicTacToe/Program.cs
@@ -10,7 +10,41 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
-            int test = 1;
+            //******************************************
+            //              TEMPORARY TEST
+            //******************************************
+            BoardState board = new BoardState();
+            BoardDisplay displayer = new BoardDisplay();
+            WinChecker checker = new WinChecker();
+
+            board.generateBoard(3);
+            Console.WriteLine(displayer.display(board));
+            bool stop = false;
+            string[] user = new string[] { "X", "O" };
+            string currentUser;
+            int turn = 0;
+            string index;
+            while (stop == false)
+            {
+                turn++;
+                currentUser = user[turn % 2];
+                Console.WriteLine("Joueur " + currentUser + " entrez un numéro de case");
+                index = Console.ReadLine();
+
+                if (index != "")
+                {
+                    board.updateBoard(Convert.ToInt32(index), currentUser);
+                    checker.haveWinner(board);
+                }
+                else
+                {
+                    stop = true;
+                }
+                Console.WriteLine(displayer.display(board));
+
+            }
+            Console.ReadLine();
+
         }
     }
 }
