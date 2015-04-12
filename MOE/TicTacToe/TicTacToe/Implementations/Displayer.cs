df0bae5959421cd79ca5fc16e90e69399d0b5f12
Clément Hémidy <clement@hemidy.fr>
2015-04-12 17:47:41 +0200

    Add Displayer.cs

---
+++ MOE/TicTacToe/TicTacToe/Implementations/Displayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TicTacToe
+{
+	public class Displayer : IDisplayer
+	{
+		public virtual void Show(string s, ConsoleColor color = ConsoleColor.White, bool mustAddCarriageReturn = true)
+		{
+			if(color != null)
+				Console.ForegroundColor = color;
+
+			if (mustAddCarriageReturn)
+				Console.WriteLine(s);
+			else
+				Console.Write(s);
+
+			Console.ResetColor ();
+		}
+		public virtual void Clear()
+		{
+			Console.Clear();
+		}
+	}
+}
+
