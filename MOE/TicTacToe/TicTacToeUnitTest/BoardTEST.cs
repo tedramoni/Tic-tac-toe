16dd89c334c8a9371100d11e81dc7c5c0a5407c1
Clément Hémidy <clement@hemidy.fr>
2015-05-04 14:25:15 +0200

    Changement projet de test

---
+++ MOE/TicTacToe/TicTacToeUnitTest/BoardTEST.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using TicTacToe;
+
+namespace TicTacToeTST
+{
+	[TestFixture ()]
+	public class BoardTEST
+	{
+		[Test ()]
+		public void playTurnTest ()
+		{
+			string errMsg;
+			int boardSize = 3;
+			Player player1;
+			Player[][] player;
+			Board boardTEST;
+			//L'objet PrivateObject permet d'acceder aux methodes et attributs
+			//normalement prives de l'objet clone
+
+			player1 = new Player("Bob", "O");
+			boardTEST = new Board(boardSize);
+
+			for (int i = 1; i <= 9;i++ )
+			{
+				boardTEST.playTurn(i, player1);
+				player = boardTEST.BoardState;
+				var length = player.GetLength (0);
+				errMsg = "Erreur : playTurn n'a pas fonctionné sur la cellule "+i;
+				if (i % length == 0){
+					Assert.IsTrue(player[i/length-1][length-1]==player1,errMsg);
+				} 
+				else {
+					Assert.IsTrue(player[(int)Math.Truncate((double)(i / length))][i % length - 1] == player1, errMsg);
+				}
+			}
+
+		}
+	}
+}
+
